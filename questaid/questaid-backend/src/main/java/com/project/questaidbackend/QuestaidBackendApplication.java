ec10de4e00a7add85b0e9d50697e641a23265277
S-M-J-I <jishanlion@gmail.com>
2023-08-29 21:37:40 +0600

    init commit

---
+++ questaid/questaid-backend/src/main/java/com/project/questaidbackend/QuestaidBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.questaidbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestaidBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestaidBackendApplication.class, args);
+	}
+
+}
---
+++ questaid/questaid-backend/src/test/java/com/project/questaidbackend/QuestaidBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.questaidbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestaidBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
