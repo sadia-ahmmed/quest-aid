4048d8cce8800a5b1a8c05ad2e61d4de940341c4
S-M-J-I <jishanlion@gmail.com>
2024-10-14 20:52:57 +0600

    updated student unwraping

--- model-services/models/llm_dir.py
+++ model-services/models/llm_dir.py
@@ -1,12 +1,10 @@
-from langchain.llms import OpenAI, GooglePalm
-from langchain.chat_models import ChatOpenAI, ChatGooglePalm
-from configs import OPENAI_API_KEY, GOOGLE_API_KEY
-from models.customllm import CustomLLM
-from langchain.cache import InMemoryCache
+from langchain_community.llms.google_palm import GooglePalm
+from langchain_community.chat_models import ChatGooglePalm
+from configs import GOOGLE_API_KEY
+from langchain_community.cache import InMemoryCache
 
 llm_cache = InMemoryCache()
-openai_gpt = OpenAI(openai_api_key=OPENAI_API_KEY) # 
-chat_gpt_openai = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
-custom_llm = CustomLLM(n=100)
+# openai_gpt = OpenAI(openai_api_key=OPENAI_API_KEY)
+# chat_gpt_openai = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
 google_palm = GooglePalm(google_api_key=GOOGLE_API_KEY)
 google_palm_chat = ChatGooglePalm(google_api_key=GOOGLE_API_KEY)
--- questaid-backend/src/main/java/com/project/questaidbackend/exceptions/EntityNotFoundException.java
+++ questaid-backend/src/main/java/com/project/questaidbackend/exceptions/EntityNotFoundException.java
@@ -4,6 +4,10 @@ import java.util.Locale;
 
 public class EntityNotFoundException extends RuntimeException{
     public EntityNotFoundException(Long id, Class<?> entity) {
-        super(entity.getSimpleName().toLowerCase(Locale.ROOT) + " with " + id + " was not found");
+        super(entity.getSimpleName().toLowerCase(Locale.ROOT) + " with ID:" + id + " was not found");
+    }
+
+    public EntityNotFoundException(String name, Class<?> entity) {
+        super(entity.getSimpleName().toLowerCase(Locale.ROOT) + " with Name:" + name + " was not found");
     }
 }
